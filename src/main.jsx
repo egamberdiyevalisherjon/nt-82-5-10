6e6bf69525eeb2f53ae4be059d8515b917f97095
Alisherjon Egamberdiyev <egamberdiyevalisherjon@gmail.com>
2023-02-11 11:45:42 +0500

    axios defaults

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
+// Axios
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:5500/api/v1";
+axios.defaults.headers.common["Content-Type"] = "application/json";
+// axios.defaults.headers.common["x-auth-token"] = `Bearer ${token}`
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <Provider store={store}>
